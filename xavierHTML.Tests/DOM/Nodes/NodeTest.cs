cecfd20de01573c3ca743c002d75fc0832551313
Chance Snow <git@chancesnow.me>
2018-10-04 00:26:04 -0700

    Add Tests project for xavierHTML

---
+++ xavierHTML.Tests/DOM/Nodes/NodeTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace xavierHTML.Tests.DOM.Nodes
+{
+    public class NodeTest
+    {
+        [Fact]
+        public void NodeHasParentTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
