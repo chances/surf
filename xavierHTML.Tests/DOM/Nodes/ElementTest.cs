8f89f27b3570c64eb5a5701e13cf01f134adf82d
Chance Snow <git@chancesnow.me>
2018-10-04 00:32:55 -0700

    Add NodeHasParent test

---
+++ xavierHTML.Tests/DOM/Nodes/ElementTest.cs
@@ -0,0 +1,20 @@
+﻿using xavierHTML.DOM;
+using xavierHTML.DOM.Elements;
+using Xunit;
+
+namespace xavierHTML.Tests.DOM.Nodes
+{
+    public class ElementTest
+    {
+        [Fact]
+        public void NodeHasParentTest()
+        {
+            var parent = new Element("node");
+            var child = new Element("node");
+            parent.Children.Add(child);
+
+            Assert.True(child.Parent.Equals(parent));
+            Assert.True(child.ParentElement.Equals(parent));
+        }
+    }
+}
