b947791109e4cf92ef003c99adfc39a95d6f6e6f
Chance Snow <git@chancesnow.me>
2019-02-01 03:38:18 -0800

    Add constants for the 16 basic CSS colors

--- xavierHTML/CSS/Values/Color.cs
+++ xavierHTML/CSS/Values/Color.cs
@@ -6,9 +6,22 @@ namespace xavierHTML.CSS.Values
     public struct Color
     {
         public static readonly Color Transparent = new Color(0, 0, 0, 0);
-        public static readonly Color Red = new Color(255, 0, 0);
-        public static readonly Color Green = new Color(0, 255, 0);
-        public static readonly Color Blue = new Color(0, 0, 255);
+        public static readonly Color Black = new Color("000000");
+        public static readonly Color Silver = new Color("C0C0C0");
+        public static readonly Color Gray = new Color("808080");
+        public static readonly Color White = new Color("FFFFFF");
+        public static readonly Color Maroon = new Color("800000");
+        public static readonly Color Red = new Color("FF0000");
+        public static readonly Color Purple = new Color("800080");
+        public static readonly Color Fuchsia = new Color("FF00FF");
+        public static readonly Color Green = new Color("008000");
+        public static readonly Color Lime = new Color("00FF00");
+        public static readonly Color Olive = new Color("808000");
+        public static readonly Color Yellow = new Color("FFFF00");
+        public static readonly Color Navy = new Color("000080");
+        public static readonly Color Blue = new Color("0000FF");
+        public static readonly Color Teal = new Color("008080");
+        public static readonly Color Aqua = new Color("00FFFF");
 
         public readonly byte R;
         public readonly byte G;
