267c04666b21878d70efc76a6be449d55d862429
Chance Snow <git@chancesnow.me>
2024-07-23 18:41:27 -0500

    Add an app icon

--- Surf/MainWindow.cs
+++ Surf/MainWindow.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Resources;
 #if Windows
+using System.Drawing;
 using System.Windows.Forms;
 #elif OSX
 using AppKit;
@@ -12,7 +16,21 @@ namespace Surf
 {
 #if Windows
 	public partial class MainWindow : Form
-	{}
+	{
+		static string[] cultureNames = { "en-US", "fr-FR", "es-ES" };
+
+		public MainWindow()
+		{
+			var reader = new ResXResourceReader("Resources.resx", new [] { typeof(MainWindow).Assembly.GetName() });
+			Console.WriteLine("Resources:");
+			foreach (DictionaryEntry d in reader) Console.WriteLine(d.Key.ToString() + ":\t" + d.Value.ToString());
+
+			var icon = reader.Cast<DictionaryEntry>().FirstOrDefault((DictionaryEntry entry) => entry.Key == "Icon").Value;
+			reader.Close();
+			// var resources = new ResourceManager("Resources", typeof(MainWindow).Assembly);
+			Icon = icon as Icon;
+		}
+	}
 #elif OSX
 	public class MainWindow : NSWindow
 	{
